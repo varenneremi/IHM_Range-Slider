6d088eea19e08c653d038df432b222dcbf834e9e
remiv <remi.varenne@etu.univ-grenoble-alpes.fr>
2019-10-04 11:18:10 +0200

    Creation of the interface model

--- src/rangeSlider/IRangeSliderModel.java
+++ src/rangeSlider/IRangeSliderModel.java
@@ -1,5 +1,28 @@
 package rangeSlider;
 
+import javax.swing.event.ChangeListener;
+
 public interface IRangeSliderModel {
+	
+	int getMinimum(Boolean isRight);
+	
+	void setMinimum(Boolean isRight, int newMinimmum);
+	
+	int getMaximum(Boolean isRight);
+	
+	void setMaximum(Boolean isRight, int newMinimmum);
+	
+	int getValue(Boolean isRight);
+	
+	void setValue(Boolean isRight, int newValue);
+	
+	void setRangeProperties(Boolean isRight, int value, int min, int max);
+	
+	void addChangeListener(ChangeListener x);
+	
+	void removeChangeListener(ChangeListener x);
+	
+	
+	
 
 }
